0b2fa10281f6ef3fea24dd112e4c48328f13befc
Jeel <jeelmaheshwari812@gmail.com>
2024-07-02 09:46:31 +0530

    Working on sidemenu

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -8,6 +8,17 @@ const LeftNav = () => {
 
   const { selectedCategory, setSelectedCategory, mobileMenu} = useContext(Context);
 
+  const clickHandler =(name, type) => {
+    switch (type) {
+      case "category":
+        
+        break;
+    
+      default:
+        break;
+    }
+  }
+
   return (
     <div className="md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240] md:translate-x-0 transition-all">
       <div className="flex px-5 flex-col">
@@ -27,6 +38,10 @@ const LeftNav = () => {
             </React.Fragment>
           )
         })}
+        <hr className='my-5 border-white/[0.2]' />
+        <div className="text-white/[0.5] text-[12px]">
+            Clone by: Jeel Maheshwari
+        </div>
       </div>
     </div>
   )
