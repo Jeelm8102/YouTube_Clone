0183138a4ea9539f8f4a3ebe0905ddc6b5126f72
Jeel <jeelmaheshwari812@gmail.com>
2024-07-04 09:25:17 +0530

    working on fetching data for video section

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+
+import { BsFillCheckCircleFill } from 'react-icons/bs'
+import { AiOutlineLike } from 'react-icons/ai'
+
+import {Context} from '../context/contextApi'
+import SuggestionVideoCard from './SuggestionVideoCard'
+import { useParams } from 'react-router-dom'
+import { fetchDataFromApi } from '../utils/api'
 
 const VideoDetails = () => {
+
+  const [video, setVideo] = useState()
+  const [relatedVideos, setRelatedVideo] = useState()
+  const {id} = useParams()
+  const {setLoading} = useContext( Context )
+
+  useEffect(()=>{
+    fetchVideoDetails()
+  },[id])
+
+  const fetchVideoDetails =()=>{
+    setLoading(true)
+    fetchDataFromApi(`video/details/?id=${id}`).then((res)=>{
+      console.log(res)
+      setVideo(res)
+      setLoading(false)
+    })
+  }
+
   return (
     <div>VideoDetails</div>
   )
